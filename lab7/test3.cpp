ff7cdf02b29b073b855ef5808ad52bc754799276
casey1425 <leedh1425@naver.com>
2024-10-21 14:23:00 +0900

    Add lab7-1~3

---
+++ lab7/test3.cpp
@@ -0,0 +1,67 @@
+#include <atomic>
+#include <condition_variable>
+#include <cstdlib>
+#include <iostream>
+#include <mutex>
+#include <thread>
+
+using namespace std;
+
+const int NO_JOB = -1;
+
+atomic<bool> quit(false);
+
+mutex queMutex;
+condition_variable queFillable;
+condition_variable queFilled;
+int que[1] = { NO_JOB };
+
+void producer() {
+  cout << "Producer starting. Thread id: " << this_thread::get_id() << endl;
+
+  while (quit.load() == false) {
+    int job = rand() % 100;{
+        unique_lock<mutex> ul(queMutex);//mutex lock
+    if (que[0] == NO_JOB) {
+      que[0] = job;
+      }
+    }
+  }
+  cout << "Producer finished" << endl;
+}
+
+void consumer() {
+  cout << "Consumer starting. Thread id: " << this_thread::get_id() << endl;
+
+  while (quit.load() == false) {
+    int job;{
+        unique_lock<mutex> ul(queMutex);//mutex lock
+    job = que[0];
+    que[0] = NO_JOB;
+    }
+  }
+  cout << "Consumer finshed" << endl;
+}
+
+
+int main() {
+  cout << "Main thread started. Thread id: " << this_thread::get_id() << endl;
+
+  srand(time(NULL));
+
+  thread t1(producer);
+  thread t2(consumer);
+
+  this_thread::sleep_for(chrono::seconds(5));
+  quit.store(true);
+
+  if (t1.joinable()) {
+    t1.join();
+  }
+
+  if (t2.joinable()) {
+    t2.join();
+  }
+
+  cout << "Main thread finished" << endl;
+}
